bc532873a4d297fb50850e77cde20ee4afe6a889
davidcr08 <68881672+davidcr08@users.noreply.github.com>
2025-09-06 03:03:34 -0500

    html basico

---
+++ src/app/componentes/crud-productos/crud-productos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-productos',
+  imports: [],
+  templateUrl: './crud-productos.html',
+  styleUrl: './crud-productos.css'
+})
+export class CrudProductos {
+
+}
